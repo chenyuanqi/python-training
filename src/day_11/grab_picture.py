e4b995ffe088182b10737c5b139f2af8cbf1338f
chenyuanqi <chenyuanqi90s@163.com>
2018-02-12 19:09:09 +0800

    alter the grab picture demo.

--- src/day_11/grab_picture.py
+++ src/day_11/grab_picture.py
@@ -6,7 +6,6 @@ from __future__ import print_function
 import os
 import re
 import urllib
-from urllib import request
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
@@ -26,13 +25,14 @@ def get_image_url(url):
     result = []
 
     for link in links:
-        href_content = link.get("href")
+        href_content = link.get("src")
         if href_content:
-            result.push(href_content)
+            result.append(href_content)
 
     return list(set(result))
 
-def download_image(url, dir_name = "download"):
+
+def download_image(url, dir_name="download"):
     """ 分析提取 image 链接
     Args:
         url: 要下载的 url
@@ -45,22 +45,32 @@ def download_image(url, dir_name = "download"):
     try:
         if not os.path.exists(dir_name):
             os.mkdir(dir_name)
-    except:
+    except BaseException:
         print("Failed to create directory for download image.")
         exit()
 
     for pic_url in url:
+        # 链接无效，则执行下一轮
+        if not re.match(r'^https?:/{2}\w.+$', pic_url):
+            continue
+
         pic_data = urllib.request.urlopen(pic_url).read()
         pic_name = re.split("/", pic_url)[-1]
+
+        # 图片名过长，截取后 80 个字符
+        if len(pic_name) > 80:
+            pic_name = pic_name[-80:]
+
         pic_path = dir_name + "/" + pic_name
 
         with open(pic_path, "wb") as f:
             f.write(pic_data)
 
+
 def main():
     target_link = "http://tieba.baidu.com/p/2166231880"
     grab_links = get_image_url(target_link)
-    print(grab_links)
+    download_image(grab_links)
 
 
 if __name__ == "__main__":
